c8ed7198f2012ac045a4b5650efb01516faca189
princeemeka965 <anyanwue4@gmail.com>
2023-05-10 15:46:12 +0100

    building our editor

---
+++ src/components/EditorComponents.vue
@@ -0,0 +1,151 @@
+<script setup>
+import { AnFilledStar } from '@kalimahapps/vue-icons'
+import { BxSolidSearchAlt2 } from '@kalimahapps/vue-icons'
+import { HiSolidInformationCircle } from '@kalimahapps/vue-icons'
+import { IoSharpInformationCircle } from '@kalimahapps/vue-icons'
+import { AkBookOpen } from '@kalimahapps/vue-icons'
+import { BxSolidBell } from "@kalimahapps/vue-icons";
+import { BsCheckCircleFill } from "@kalimahapps/vue-icons";
+import { HiSolidMapPin } from "@kalimahapps/vue-icons";
+
+const emit = defineEmits(['initiateDrag'])
+
+function initiateDrag(ev) {
+  emit('initiateDrag', ev.target)
+}
+</script>
+
+<template>
+  <div>
+      <!--- Display list of Html Elements that can be dragged to the popup-->
+      <div class="my-2 p-2 border border-dashed">
+        <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+          <h1 class="font-bold text-black" data-text data-header draggable="true">Header</h1>
+        </div>
+      </div>
+
+      <div class="my-2 p-2 border border-dashed">
+        <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+          <input
+            class="font-bold text-black"
+            data-text
+            disabled
+            draggable="true"
+            value="Textfield"
+          />
+        </div>
+      </div>
+
+      <div class="my-2 p-2 border border-dashed">
+        <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+          <button class="font-bold text-black" data-text disabled draggable="true">Button</button>
+        </div>
+      </div>
+
+      <div class="my-2 p-2 border border-dashed">
+        <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+          <select
+            class="font-bold text-black"
+            style="width: 100%"
+            data-select
+            disabled
+            draggable="true"
+          >
+            <option selected>Select</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="my-2 p-2 border border-dashed">
+        <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+          <input
+            class="font-bold text-black"
+            data-text
+            disabled
+            draggable="true"
+            value=""
+            placeholder="Number"
+            type="number"
+          />
+        </div>
+      </div>
+
+      <div class="my-2 p-2 border border-dashed">
+        <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+          <input
+            class="font-bold text-black"
+            data-text
+            disabled
+            draggable="true"
+            placeholder="Date Field"
+            type="text"
+            onfocus="(this.type = 'date')"
+          />
+        </div>
+      </div>
+
+      <div class="my-2 p-2 border border-dashed">
+        <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+          <textarea
+            class="font-bold text-black"
+            data-text
+            disabled
+            draggable="true"
+            placeholder="Text Area"
+            rows="1"
+          />
+        </div>
+      </div>
+
+      <div class="my-4 p-4 border">
+        <h1 class="font-black font-semibold text-lg text-black">Icons</h1>
+        <h1 class="font-semibold textbase my-2 text-black">Drag an icon to the dropzone area</h1>
+
+        <div class="flex flex-wrap w-full my-4">
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <AnFilledStar class="icons" />
+            </div>
+          </div>
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <BxSolidSearchAlt2 class="icons" />
+            </div>
+          </div>
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <HiSolidInformationCircle class="icons" />
+            </div>
+          </div>
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <IoSharpInformationCircle class="icons" />
+            </div>
+          </div>
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <AkBookOpen class="icons" />
+            </div>
+          </div>
+
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <BxSolidBell class="icons" />
+            </div>
+          </div>
+
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <BsCheckCircleFill class="icons" />
+            </div>
+          </div>
+
+          <div class="p-1 mx-2 flex poptin-editor">
+            <div class="w-full" draggable="true" @dragstart="initiateDrag($event)">
+              <HiSolidMapPin class="icons" />
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
--- src/views/EditorView.vue
+++ src/views/EditorView.vue
@@ -1,11 +1,80 @@
 <script>
+import { ref } from 'vue'
+import EditorMenu from '../components/EditorMenu.vue'
+import TextFormatEditor from '../components/TextFormatEditor.vue'
 export default {
-    setup() {
-        
-    },
+  components: { TextFormatEditor, EditorMenu },
+  setup() {
+    const draggedElem = ref(null)
+
+    const allowDrop = (event) => {
+      event.preventDefault()
+    }
+
+    const drop = (event) => {
+      event.preventDefault();
+      event.target.appendChild(draggedElem.value.cloneNode(true));
+
+      const domLength = document.querySelector('[data-dropzone]').querySelectorAll('[data-text]').length;
+
+      if (draggedElem.value.disabled) {
+        for (let i = 0; i < domLength; i++) {
+          document.querySelector('[data-dropzone]').querySelectorAll('[data-text]')[i].removeAttribute('disabled')
+          // Add basic css class for styling
+          document.querySelector('[data-dropzone]').querySelectorAll('[data-text]')[i].classList.add('p-2');
+          document.querySelector('[data-dropzone]').querySelectorAll('[data-text]')[i].classList.add('border');
+        }
+      }
+      else {
+        for (let i = 0; i < domLength; i++) {
+          // Add basic css class for styling
+          document.querySelector('[data-dropzone]').querySelectorAll('[data-text]')[i].classList.add('p-2');
+
+          // check for headers and make them editable
+          if (document.querySelector('[data-dropzone]').querySelectorAll('[data-text]')[i].hasAttribute('data-header')) {
+            document.querySelector('[data-dropzone]').querySelectorAll('[data-text]')[i].setAttribute('contentEditable', true)
+          }
+        }
+      }
+    }
+
+    return {
+      allowDrop,
+      drop,
+      draggedElem
+    }
+  }
 }
 </script>
 
 <template>
-    <div />
+  <div class="w-full flex flex-col">
+    <header class="w-full p-5 flex">
+      <img src="@/assets/poptin.png" width="100" />
+    </header>
+
+    <section class="w-full h-full flex justify-center">
+      <div
+        class="lg:w-11/12 w-full flex sm:flex-col md:flex-col lg:flex-row bg-white my-5 lg:p-5 p-2"
+      >
+        <!--- Section for the canvas -->
+
+        <div class="lg:w-2/3 flex w-full flex-col lg:mx-3">
+          <text-format-editor />
+
+          <div
+            class="w-full flex flex-grow border border-dashed flex-col p-5 my-2"
+            @drop="drop($event)"
+            @dragover="allowDrop($event)"
+            data-dropzone
+          >
+        </div>
+        </div>
+
+        <div class="lg:w-1/3 w-full flex flex-col p-2 poptin-editor">
+          <editor-menu @selectedElem="draggedElem = $event" />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
